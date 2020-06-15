d6f6c92f2a99056c7c6db731425f5b83fb0b940f
MaryamMosstoufi <maryam@zooxoos.com>
2020-06-15 17:45:52 -0400

    MVP + Stretch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,25 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import MovieCard from './MovieCard.js';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+  const { movieId } = useParams();
+
   useEffect(() => {
-    const id = 1;
+
+    // match loosely because the id is a number, and the itemId from URL is a string
+
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
+    axios
+      .get(`http://localhost:5000/api/movies/${movieId}`)
+      .then(response => {
+        setMovie(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
 
-  },[]);
-  
+  }, []);
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
@@ -32,25 +36,26 @@ const Movie = (props) => {
 
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
+    // <div className="save-wrapper">
+    //   <div className="movie-card">
+    //     <h2>{title}</h2>
+    //     <div className="movie-director">
+    //       Director: <em>{director}</em>
+    //     </div>
+    //     <div className="movie-metascore">
+    //       Metascore: <strong>{metascore}</strong>
+    //     </div>
+    //     <h3>Actors</h3>
 
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
+    //     {stars.map(star => (
+    //       <div key={star} className="movie-star">
+    //         {star}
+    //       </div>
+    //     ))}
+    //   </div>
+    //   <div className="save-button">Save</div>
+    // </div>
+    <MovieCard key={movie.id} movie={movie} />
   );
 }
 
